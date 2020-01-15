6cbf74caa52afa19250a7221a1571b6f0121ea15
Aleksandr Karamyshev <sheckspir88@gmail.com>
2020-01-15 10:48:55 -0300

    check flexbox bug for new version of library

--- app/src/main/java/com/example/liangtg/dev/MainActivity.java
+++ app/src/main/java/com/example/liangtg/dev/MainActivity.java
@@ -1,9 +1,10 @@
 package com.example.liangtg.dev;
 
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Point;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -11,6 +12,10 @@ import android.widget.TextView;
 import com.google.android.flexbox.FlexDirection;
 import com.google.android.flexbox.FlexWrap;
 import com.google.android.flexbox.FlexboxLayoutManager;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
@@ -24,11 +29,19 @@ public class MainActivity extends AppCompatActivity {
         layoutManager.setFlexWrap(FlexWrap.WRAP);
         layoutManager.setFlexDirection(FlexDirection.COLUMN);
         recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setAdapter(new IAdapter());
+        Point point = new Point();
+        getWindowManager().getDefaultDisplay().getSize(point);
+        // TODO KAY определить как layoutManager говорит что нужно скролиться или нет
+        recyclerView.setAdapter(new IAdapter(point.x));
     }
 
     private class IAdapter extends RecyclerView.Adapter<AdapterViewHolder> {
         int[] colors = {0xFFFF0000, 0xFF00FF00, 0xFF0000FF};
+        private int screenWidth;
+
+        public IAdapter(int screenWidth) {
+            this.screenWidth = screenWidth;
+        }
 
         @NonNull
         @Override
@@ -39,6 +52,9 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onBindViewHolder(@NonNull AdapterViewHolder holder, int position) {
             FlexboxLayoutManager.LayoutParams lp = (FlexboxLayoutManager.LayoutParams) holder.itemView.getLayoutParams();
+            // TODO KAY posible hot fix
+            Log.d("TAG", "onBindViewHolder.position = " + position + " lp width = " + lp.width);
+//            lp.width = screenWidth;
             holder.itemView.setBackgroundColor(colors[position % colors.length]);
             holder.title.setText("title" + position);
             holder.content.setText("content" + position);
